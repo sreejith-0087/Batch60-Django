ab2b978f69da283764f564db287154cee6d1be8c
sreejith-0087 <sreejith.s.8700@gmail.com>
2025-03-05 10:48:11 +0530

    Static & Page Redirection

--- Project1/app1/urls.py
+++ Project1/app1/urls.py
@@ -8,5 +8,12 @@ urlpatterns = [
     path('wishes2/', Wishes2, name='wishes2'),
     path('std_name/<str:nme>', Std_Name, name='std_name'),
     path('std_age/<int:age>', Std_Age, name='std_age'),
-    path('', Base_Html, name='base_html'),
+    path('base_html/', Base_Html, name='base_html'),
+    path('static_file/', Static_File, name='static_file'),
+    path('', Page_Redirection_Home, name='home'),
+    path('about/', Page_Redirection_About, name='about'),
+    path('contact/', Page_Redirection_Contact, name='contact'),
 ]
+
+
+
--- Project1/app1/views.py
+++ Project1/app1/views.py
@@ -17,3 +17,16 @@ def Std_Age(request, age):
 
 def Base_Html(request):
     return render(request, '1.Base_Page.html')
+
+def Static_File(request):
+    return render(request, '2.Static_File.html')
+
+def Page_Redirection_Home(request):
+    return render(request, '3.Page_Redirection(Home).html')
+
+def Page_Redirection_About(request):
+    return render(request, '4.Page_Redirection(About).html')
+
+def Page_Redirection_Contact(request):
+    return render(request, '5.Page_Redirection(Contact).html')
+
